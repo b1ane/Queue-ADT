7701c7726d08e7af7b39f29c07f35f651a197835
Blane Zewdie <b1ane@outlook.com>
2022-09-25 14:35:00 -0500

    Initial Commit

---
+++ Queue ADT/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Queue ADT
+//
+//  Created by blane on 9/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
